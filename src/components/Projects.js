50a3b66b2a94afc1615701ee75850b18e8d5ac37
Treyceratops <treytowers@yahoo.com>
2023-02-27 13:12:47 -0800

    update transform/scale for mobile and home logo's width

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,9 @@ function Projects(props) {
 	return (
 		<div>
 			<style>{'.nav-projects { font-weight: bold; }'}</style>
+			<h1>
+				This page is currently under construction and will be available shortly
+			</h1>
 		</div>
 	);
 }

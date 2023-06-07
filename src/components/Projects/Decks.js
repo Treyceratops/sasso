576ec3eee377b3e0f6c647a2acb16065567106fd
Treyceratops <treytowers@yahoo.com>
2023-06-07 11:12:22 -0700

    add more deck pics

--- src/components/Projects/Decks.js
+++ src/components/Projects/Decks.js
@@ -3,6 +3,9 @@ import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import Pic1 from '../../images/decks/home-design.jpeg';
+import Pic2 from '../../images/decks/home-design-2.jpeg';
+import Pic3 from '../../images/decks/home-design-3.jpeg';
+import Pic4 from '../../images/decks/home-design-4.jpeg';
 
 function Decks(props) {
 	useEffect(() => {
@@ -15,6 +18,9 @@ function Decks(props) {
 				{'.nav-projects { font-weight: bold; }'}
 			</style>
 			<img data-aos='fade-up' src={Pic1} alt='deck-pic' />
+			<img data-aos='fade-up' src={Pic2} alt='deck-pic' />
+			<img data-aos='fade-up' src={Pic3} alt='deck-pic' />
+			<img data-aos='fade-up' src={Pic4} alt='deck-pic' />
 		</div>
 	);
 }
